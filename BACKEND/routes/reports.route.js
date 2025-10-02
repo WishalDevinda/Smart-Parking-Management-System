56c636ddec97ffa4ebd70b2eba4937571a3756be
WishalDevinda <wdkg2003@gmail.com>
2025-10-02 16:51:37 +0530

    Delete BACKEND directory

--- BACKEND/routes/reports.route.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-const r = require('../controller/reports.controller');
-router.get('/usage', r.usage);
-router.get('/maintenance', r.maintenance);
-module.exports = router;
